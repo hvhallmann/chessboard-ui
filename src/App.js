8b6e69e3c2ae08ab197ccdee154abd4f59ec3602
Henrique Hallmann <henrique_hallmann@hotmail.com>
2019-08-31 11:00:33 -0300

    fix naming convention

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import Partners from './pages/Partners/Users';
+import Users from './pages/Users/Users';
 import Index from './pages/Index';
 
 const App = () => (
   <Router>
     <Route path='/' exact component={Index} />
-    <Route path='/users/' component={Partners} />
+    <Route path='/users/' component={Users} />
   </Router>
 );
 
