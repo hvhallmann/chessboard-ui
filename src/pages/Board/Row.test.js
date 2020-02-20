8987a71451dc5662bc083308dcd2ad6848904044
Henrique Hallmann <henrique_hallmann@hotmail.com>
2020-02-20 00:20:21 -0300

    include tests

--- src/pages/Board/Row.test.js
+++ src/pages/Board/Row.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Row from './Row';
 
-it('renders without crashing', () => {
+it('renders Row without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Row tile={[{ isBlack: true }]} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
--- src/pages/Board/Tile.test.js
+++ src/pages/Board/Tile.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Tile from './Tile';
 
-it('renders without crashing', () => {
+it('renders Tile without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Tile />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
